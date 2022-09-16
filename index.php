348054ece18720bc166bb99da7c2314c73e58b6e
Robster <robbom0319@gmail.com>
2022-09-16 21:57:07 +0200

    Redirects to home.php

--- index.php
+++ index.php
@@ -1,22 +1,5 @@
 <?php
-declare(strict_types=1);
-require __DIR__ . '/vendor/autoload.php';
-include('DB/_db.php');
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-
-
-$mysqldb = new _db();
-
-//$mysqldb->create("Walter", "Im Skyler white yo");
-
-$data = $mysqldb->read();
-
-print_r($data);
-
-$mysqldb->connect();
+header('Location:./pages/home.php');
 
 
 
